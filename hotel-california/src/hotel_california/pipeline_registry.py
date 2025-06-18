032e69cfc0c83ed0b318571ceb2199a4589d0020
antoniooliveira03 <tzpoliveira@gmail.com>
2025-06-18 19:00:22 +0100

    split

--- hotel-california/src/hotel_california/pipeline_registry.py
+++ hotel-california/src/hotel_california/pipeline_registry.py
@@ -1,27 +1,17 @@
 from typing import Dict
 from kedro.pipeline import Pipeline
 
-'''
+
 from project_template.pipelines import (
-  #  pipeline name as name,
+    split as data_split,
 
 )
 
 def register_pipelines() -> Dict[str, Pipeline]:
 
-    unit_tests_stage = unit_tests.create_pipeline()
-    feature_store = feature_store.create_pipeline()
-    unit_tests_2 = unit_tests_2.create_pipeline()
+    data_split = data_split.create_pipeline()
     # ...
 
     return {
-        "unit_tests": unit_tests_stage,
-        "preprocessing": preprocessing_stage,
-        "split_data": split_data_stage,
-        "feature_selection": feature_selection_stage,
-        "tuning" : model_tuning_stage,
-        "train": model_train_stage,
-        "predict": model_predict_stage, 
-        "drift_test" : drift_test_stage, 
-        "__default__": preprocessing_stage + split_data_stage + feature_selection_stage + model_tuning_stage + model_train_stage,
-    }'''
+        "split": data_split,
+    }
--- hotel-california/src/hotel_california/pipelines/01_data_split/nodes.py
+++ hotel-california/src/hotel_california/pipelines/01_data_split/nodes.py
@@ -1,51 +1,30 @@
-import logging
-from typing import Any, Dict, Tuple
-import numpy as np
+from typing import Dict, Any, Tuple
 import pandas as pd
-import great_expectations as ge
-import mlflow
 
-def unit_test(data: pd.DataFrame,): 
-
-    if mlflow.active_run():
-        mlflow.end_run()
-
-    df=data.copy(deep=True)
-
-    mlflow.set_experiment("data_unit_tests")
-
-    with mlflow.start_run(run_name="verify_data_quality") as run:
-        mlflow.set_tag("mlflow.runName", "verify_data_quality")
+def split_data(data: pd.DataFrame, parameters: Dict[str, Any]) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Series, pd.Series]:
     
-        # Log the raw data statistics
-        describe_to_dict=df.describe().to_dict()
-        mlflow.log_dict(describe_to_dict,"describe_data_raw.json")
+    df = data.copy()
+    if "ArrivalYear" not in df.columns:
+        raise ValueError("DataFrame must contain 'ArrivalYear' column for chronological sorting.")
+    else:
+        df = df.sort_values("ArrivalYear") 
     
-        # Perform data quality checks using Great Expectations
-        pd_df_ge = ge.from_pandas(df)
-        assert pd_df_ge.expect_column_values_to_be_between('product_weight_g', min_value=0, max_value=50000, mostly=0.75).success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('product_weight_g').success == False
-        assert pd_df_ge.expect_column_values_to_be_between('product_length_cm', min_value=5, max_value=150, mostly=0.75).success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('product_length_cm').success == False
-        assert pd_df_ge.expect_column_values_to_be_between('product_width_cm', min_value=5, max_value=110, mostly=0.75).success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('product_width_cm').success == False
-        assert pd_df_ge.expect_column_values_to_be_between('product_height_cm', min_value=5, max_value=110, mostly=0.75).success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('product_height_cm').success == False
-        assert pd_df_ge.expect_column_to_exist('order_status').success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('order_status').success == True
-        assert pd_df_ge.expect_column_to_exist('price').success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('customer_id').success == True
-        assert pd_df_ge.expect_column_values_to_not_be_null('product_category_name').success == False
-        assert pd_df_ge.expect_column_values_to_be_in_set("order_status", ['delivered', 'shipped', 'processing', 'canceled', 'invoiced', 'unavailable', 'approved']).success == False
-        assert pd_df_ge.expect_column_values_to_be_in_type_list("customer_id", ["int", "int64"]).success == True
-        
-         # Log the cleaned data statistics
-        describe_to_dict=df.describe().to_dict()
-        mlflow.log_dict(describe_to_dict,"describe_data_cleaned.json")
-    mlflow.end_run()
-
-
-    log = logging.getLogger(__name__)
-    log.info("Data passed on the unit data tests")
-
-    return 0
+    # Extract target as Series
+    target = df['Canceled']
+    
+    # Optional
+    # numeric = df.select_dtypes(include=['number']).drop(columns=["Canceled"])
+    # notnumeric = df.select_dtypes(exclude=['number']).columns.tolist()
+    
+    # Drop target from features
+    df = df.drop(columns=["Canceled"])
+    
+    # Chronological split
+    split_idx = int(0.8 * len(df))
+    X_train_data = df.iloc[:split_idx]
+    X_val_data = df.iloc[split_idx:]
+    
+    y_train_data = target.iloc[:split_idx]
+    y_val_data = target.iloc[split_idx:]
+    
+    return X_train_data, X_val_data, y_train_data, y_val_data
