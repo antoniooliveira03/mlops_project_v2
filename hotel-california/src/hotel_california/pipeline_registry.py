1824ad48c2b8fb1dc9b443c98e634f6735dd096a
antoniooliveira03 <tzpoliveira@gmail.com>
2025-06-18 18:38:38 +0100

    esqueleto pipeline registry

--- hotel-california/src/hotel_california/pipeline_registry.py
+++ hotel-california/src/hotel_california/pipeline_registry.py
@@ -1,16 +1,26 @@
-"""Project pipelines."""
-from __future__ import annotations
-
-from kedro.framework.project import find_pipelines
+from typing import Dict
 from kedro.pipeline import Pipeline
 
+from project_template.pipelines import (
+  #  pipeline name as name,
 
-def register_pipelines() -> dict[str, Pipeline]:
-    """Register the project's pipelines.
+)
 
-    Returns:
-        A mapping from pipeline names to ``Pipeline`` objects.
-    """
-    pipelines = find_pipelines()
-    pipelines["__default__"] = sum(pipelines.values())
-    return pipelines
+def register_pipelines() -> Dict[str, Pipeline]:
+
+    unit_tests_stage = unit_tests.create_pipeline()
+    feature_store = feature_store.create_pipeline()
+    unit_tests_2 = unit_tests_2.create_pipeline()
+    # ...
+
+    return {
+        "unit_tests": unit_tests_stage,
+        "preprocessing": preprocessing_stage,
+        "split_data": split_data_stage,
+        "feature_selection": feature_selection_stage,
+        "tuning" : model_tuning_stage,
+        "train": model_train_stage,
+        "predict": model_predict_stage, 
+        "drift_test" : drift_test_stage, 
+        "__default__": preprocessing_stage + split_data_stage + feature_selection_stage + model_tuning_stage + model_train_stage,
+    }
