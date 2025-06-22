f0981668fd352530ae7ecefab510186b9b09a047
AB27 <anabdfarinha@gmail.com>
2025-06-22 20:14:37 +0100

    train pipeline start

--- hotel-california/src/hotel_california/pipeline_registry.py
+++ hotel-california/src/hotel_california/pipeline_registry.py
@@ -6,7 +6,8 @@ from hotel_california.pipelines import (
     data_split_01 as data_split,
     data_unit_tests_02 as data_unit_tests,
     data_preproc_03 as data_preproc,
-    model_selection_0x as model_selection
+    model_selection_0x as model_selection,
+    model_train_0x as model_train_pipeline,
 
 )
 
@@ -15,6 +16,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     data_split_pipeline = data_split.create_pipeline()
     data_unit_tests_pipeline = data_unit_tests.create_pipeline()  
     data_preproc_pipeline = data_preproc.create_pipeline()
+    model_train_pipeline = model_train_pipeline.create_pipeline()
     model_selection_pipeline = model_selection.create_pipeline()
     # ...
 
@@ -23,6 +25,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
         "unit_tests": data_unit_tests_pipeline,
         "preproc": data_preproc_pipeline,
         "model_selection": model_selection_pipeline,
+        "model_train": model_train_pipeline,
         "__default__": data_split_pipeline + data_unit_tests_pipeline + data_preproc_pipeline,
         
     }
