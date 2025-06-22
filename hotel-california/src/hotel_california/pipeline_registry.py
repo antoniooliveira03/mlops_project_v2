cce4902763d0707fdb34a6c0d742af7419ff9ff5
AB27 <anabdfarinha@gmail.com>
2025-06-22 19:59:24 +0100

    1st try model_selection

--- hotel-california/src/hotel_california/pipeline_registry.py
+++ hotel-california/src/hotel_california/pipeline_registry.py
@@ -6,6 +6,7 @@ from hotel_california.pipelines import (
     data_split_01 as data_split,
     data_unit_tests_02 as data_unit_tests,
     data_preproc_03 as data_preproc,
+    model_selection_0x as model_selection
 
 )
 
@@ -14,11 +15,14 @@ def register_pipelines() -> Dict[str, Pipeline]:
     data_split_pipeline = data_split.create_pipeline()
     data_unit_tests_pipeline = data_unit_tests.create_pipeline()  
     data_preproc_pipeline = data_preproc.create_pipeline()
+    model_selection = model_selection.create_pipeline()
     # ...
 
     return {
         "split": data_split_pipeline,
         "unit_tests": data_unit_tests_pipeline,
         "preproc": data_preproc_pipeline,
+        "model_selection": model_selection,
         "__default__": data_split_pipeline + data_unit_tests_pipeline + data_preproc_pipeline,
+        
     }
