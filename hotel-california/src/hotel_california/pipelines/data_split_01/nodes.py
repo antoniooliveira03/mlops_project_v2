1f3b49cf0153add9a7b17c1478593625d89ebfe4
antoniooliveira03 <tzpoliveira@gmail.com>
2025-06-24 14:07:11 +0100

    update split

--- hotel-california/src/hotel_california/pipelines/data_split_01/nodes.py
+++ hotel-california/src/hotel_california/pipelines/data_split_01/nodes.py
@@ -21,8 +21,8 @@ def split_data(data: pd.DataFrame) -> Tuple[pd.DataFrame, pd.DataFrame, pd.Serie
     X_test_data = df.iloc[val_end:]
 
     # Targets
-    y_train_data = target["Canceled"].iloc[:train_end]
-    y_val_data = target["Canceled"].iloc[train_end:val_end]
-    y_test_data = target["Canceled"].iloc[val_end:]
+    y_train_data = target.iloc[:train_end]
+    y_val_data = target.iloc[train_end:val_end]
+    y_test_data = target.iloc[val_end:]
 
     return X_train_data, X_val_data, X_test_data, y_train_data, y_val_data, y_test_data
