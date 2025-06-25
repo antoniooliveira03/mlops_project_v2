764674a43faf4c6dd6df8f43f270c8f11957682d
AB27 <anabdfarinha@gmail.com>
2025-06-25 22:07:33 +0100

    typo corrected

--- hotel-california/src/hotel_california/pipeline_registry.py
+++ hotel-california/src/hotel_california/pipeline_registry.py
@@ -24,7 +24,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
     feature_selection_pipeline = feature_selection.create_pipeline()
     model_train_pipeline = model_train.create_pipeline()
     model_selection_pipeline = model_selection.create_pipeline()
-    model_prediction_pipeline = model_prediction.create_pipeline()
+    model_prediction_pipeline = model_predict.create_pipeline()
 
     return {
         "split": data_split_pipeline,
