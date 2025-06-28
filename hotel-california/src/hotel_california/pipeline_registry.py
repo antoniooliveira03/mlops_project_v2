14877ec0898e11e61d45b6d56563eef88b330b29
antoniooliveira03 <tzpoliveira@gmail.com>
2025-06-28 10:40:38 +0100

    models

--- hotel-california/src/hotel_california/pipeline_registry.py
+++ hotel-california/src/hotel_california/pipeline_registry.py
@@ -26,15 +26,17 @@ def register_pipelines() -> Dict[str, Pipeline]:
     model_selection_pipeline = model_selection.create_pipeline()
     model_prediction_pipeline = model_predict.create_pipeline()
 
+    model_selection_process = model_train_pipeline + model_selection_pipeline
+
     return {
         "split": data_split_pipeline,
         "unit_tests": data_unit_tests_pipeline,
         "preproc": data_preproc_pipeline,
         "unit_tests_afterprep": data_unit_tests_afterprep_pipeline,
         "feature_selection": feature_selection_pipeline,
-        "model_selection_process": model_train_pipeline + model_selection_pipeline,
         "model_selection": model_selection_pipeline,
         "model_train": model_train_pipeline,
+        "model_selection_process": model_selection_process,
         "model_prediction": model_prediction_pipeline,
         #"ingestion": ingestion_pipeline,
         # The ingestion pipeline is commented out by default to avoid long execution times.
@@ -43,6 +45,7 @@ def register_pipelines() -> Dict[str, Pipeline]:
                         data_unit_tests_pipeline + 
                         data_preproc_pipeline + 
                         data_unit_tests_afterprep_pipeline + 
-                        feature_selection_pipeline
+                        feature_selection_pipeline +
+                        model_selection_process,
                         #+ ingestion_pipeline,
     }
